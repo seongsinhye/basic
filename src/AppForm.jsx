5901b9bfdc6bd2ac2f87559d081e17f4cc172920
seongsinhye <sinhye2428@naver.com>
2024-05-23 23:25:21 +0900

    Form React 사용해보기 :  모든 UI의 업데이트는, 상태의 변경으로 부터 발생해야 한다

---
+++ src/AppForm.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+
+export default function AppForm() {
+  const [form, setFrom] = useState({name : '', email : ''})
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log("form", form);
+  }
+  const handleChange = (e) => {
+    console.log("target", e.target);
+    const {name,  value} = e.target;
+    setFrom({...form , [name] : value})
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='name'>이름:</label>
+      <input
+        type='text'
+        id='name'
+        name='name'
+        value={form.name}
+        onChange={handleChange}
+      />
+      <label htmlFor='email'>이메일:</label>
+      <input
+        type='email'
+        id='email'
+        name='email'
+        value={form.email}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </form>
+  );
+}
+
